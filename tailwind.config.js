58e9ad4cb2242ffbc0d71e24c81675c9467c1d51
philipNgungi037 <philipngungi037@gmail.com>
2024-02-28 08:13:39 +0300

    :wrench: Update tailwind configuration.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,52 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+ 
+  /**Configure template paths */
+  content: ["./src/**/*.{html,js,jsx,css}"],
+
+   
   theme: {
-    extend: {},
+    // colors: {
+    //   'grent': '#39edb8"',
+    // },
+
+    /**this where responsiveness will be handled customizing breakpoints*/
+    screens: {
+      xs: "480px",
+
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+    },
+
+    /**The back-ground image will be accessed directly. e.g bg-image will refer to 'img' its specific path. */
+    extend: {
+      backgroundImage:{
+        'bg-image':'url("./file-path/img")',
+      },
+      /**Fonts  */
+      fontFace: {
+        'fontawesome': {
+          'fontFamily': 'Font Awesome',
+          'src': 'url(/fonts/font-awesome/fonts/fontawesome-webfont.woff) format("woff")',
+          'fontStyle': 'normal',
+          'fontWeight': 'normal',
+          'fontDisplay': 'auto',
+        }
+      },
+      
+    },
   },
   plugins: [],
 }
-
