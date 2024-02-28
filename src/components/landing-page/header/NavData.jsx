e24156495fce430467400ce9b8368ab159e3c0d6
philipNgungi037 <philipngungi037@gmail.com>
2024-02-28 09:22:55 +0300

    :sparkles: Navdata to dynamically add navs.

---
+++ src/components/landing-page/header/NavData.jsx
@@ -0,0 +1,19 @@
+const Navs = [
+    {
+        name: 'Home',
+        link: '/'
+    },
+    {
+        name: 'Search',
+        link: 'search'
+    },
+    {
+        name:'Account',
+        link: 'account'
+    },
+    {
+        name: 'Cart',
+        link: 'cart'
+    }
+];
+export default Navs;
