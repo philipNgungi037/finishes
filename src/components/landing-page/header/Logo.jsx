9bf3a67987c141609ac43c2660c619e42695f9ff
philipNgungi037 <philipngungi037@gmail.com>
2024-02-28 09:18:09 +0300

    :sparkles: Create logo

---
+++ src/components/landing-page/header/Logo.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Logo = () => {
+    const logo = "X-project";
+    return (
+        <div className='logo'>
+            <h3>
+                {logo}
+            </h3>
+        </div>
+    )
+}
+
+export default Logo
