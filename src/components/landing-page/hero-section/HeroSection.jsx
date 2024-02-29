875aa5cfbeafb72f4b7a2c50a2aca153eab3d97c
philipNgungi037 <philipngungi037@gmail.com>
2024-02-29 22:03:21 +0300

    :art: Render category and carousel together in a flex to make up the hero-section.

--- src/components/landing-page/hero-section/HeroSection.jsx
+++ src/components/landing-page/hero-section/HeroSection.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Carousel from './Carousel'
+import CategorySection from './CategorySection'
+import './hero-section.css'
 
 const HeroSection = () => {
     return (
-        <div>HeroSection</div>
+        <div className='hero-section flex gap-4 justify-between p-16 bg-gray-100 '>
+            <CategorySection/> 
+            <Carousel/> 
+
+        </div>
     )
 }
 
