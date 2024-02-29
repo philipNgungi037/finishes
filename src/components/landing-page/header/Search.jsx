b0df81d0e97f36e83cf4d2ba146c02e4602cfd1e
philipNgungi037 <philipngungi037@gmail.com>
2024-02-29 23:38:54 +0300

    :lipstick: Change search bar color and border radius.

--- src/components/landing-page/header/Search.jsx
+++ src/components/landing-page/header/Search.jsx
@@ -13,19 +13,19 @@ const Search = () => {
     };
 
     return (
-        <div className='search flex items-center '>
-            <div className='text-center p-1 lg:p-2 text-xs sm:text-sm  bg-yellow-200'>
+        <div className='search flex items-center  '>
+            <div className='text-center p-1 lg:p-2 text-lg rounded-md'>
                 <ion-icon name="search-outline"></ion-icon>
             </div>
 
-            <input className='text-center p-1 lg:p-2 text-xs sm:text-sm bg-yellow-200  '
+            <input className='text-center p-1 lg:p-2 text-xs sm:text-sm border border-black rounded-md '
                 type="text"
                 placeholder="search for products, brands, categories... " 
                 value={searchQuery}
                 onChange={handleSearchChange}
             />
 
-            <button className='bg-yellow-200 p-1 lg:p-2 px-4 text-xs sm:text-sm ml-2 lg:ml-8' onClick={handleSearch}>SEARCH</button>
+            <button className='search-button rounded-md p-1 lg:p-2 px-4 text-xs sm:text-sm ml-2 ' onClick={handleSearch}>SEARCH</button>
         </div>
     );
 };
