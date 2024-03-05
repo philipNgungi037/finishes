e48c1977bba2e7be55ff721554a134b0ec9fafb0
philipNgungi037 <philipngungi037@gmail.com>
2024-03-05 20:25:29 +0300

    :art::lipstick: Render and import top-offers and products css file.

--- src/pages/langing-page/LandingPage.jsx
+++ src/pages/langing-page/LandingPage.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import HeroSection from '../../components/landing-page/hero-section/HeroSection'
+import AllProducts from '../../components/landing-page/products/AllProducts'
+import '../../components/landing-page/products/products.css'
+import TopOffers from '../../components/landing-page/products/TopOffers'
 
 const LandingPage = () => {
   return (
-    <div className='landing-page bg-gray-800 '>
+    <div className='landing-page bg-gray-200 h-full '>
       <HeroSection />
+      <TopOffers/>
+      {/* <AllProducts/> */}
     </div>
   )
 }
