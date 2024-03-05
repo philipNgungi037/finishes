f321f9cf343e41034a24082edee8c23ca212e180
philipNgungi037 <philipngungi037@gmail.com>
2024-03-05 13:12:44 +0300

    :sparkles::art::lipstick: Create a re-usable product card.

---
+++ src/components/landing-page/products/ProductCard.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+// A re-usable card with product prop, to be used to display single-item all over the app
+const ProductCard = ({ product }) => {
+  return (
+    <div className="product-card bg-white shadow-md relative text-xs rounded-md ">
+      <img src={product.imgUrl} alt={product.name} className='relative bg-cover rounded-md  ' />
+      <p className='discount-tag  absolute top-0 rounded-md p-1 px-2    '><strong>Off -</strong> {product.discount}%</p>
+      <div className="details p-4 text-center ">
+        <h2 className='font-extrabold  '>{product.name}</h2>
+        <p className='underline '><strong></strong> {product.category}</p>
+        <p className='text-sm'><strong> </strong> ${product.discountedPrice}</p>
+        <p className='line-through'><strong></strong> ${product.price}</p>
+        <p><strong></strong> {product.additionalParams}</p>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
