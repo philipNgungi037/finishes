97f98ef9eeb7572e984f8025b41b35e1d25e8370
philipNgungi037 <philipngungi037@gmail.com>
2024-03-05 17:04:07 +0300

    :lipstick: Remove the hero-section background.

--- src/components/landing-page/hero-section/HeroSection.jsx
+++ src/components/landing-page/hero-section/HeroSection.jsx
@@ -5,7 +5,7 @@ import './hero-section.css'
 
 const HeroSection = () => {
     return (
-        <div className='hero-section flex gap-4 justify-between p-16 bg-gray-100 '>
+        <div className='hero-section flex gap-4 justify-between p-16'>
             <CategorySection/> 
             <Carousel/> 
 
