e7b9d7b39e3627a9cac4919ada3ae926482e3ffe
philipNgungi037 <philipngungi037@gmail.com>
2024-03-05 13:07:10 +0300

    :pencil2: Make changes on img_urls and seperate.

--- src/components/landing-page/products/ProductsData.jsx
+++ src/components/landing-page/products/ProductsData.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 
 const ProductsData = [
   {
@@ -8,7 +8,7 @@ const ProductsData = [
     price: 99.99,
     discountedPrice: 79.99,
     discount: 20,
-    imgUrl: 'https://example.com/smart_watch_series_x.jpg',
+    imgUrl: 'https://www.smartwatchforless.com/wp-content/uploads/2023/03/1-3.jpg',
     additionalParams: 'Waterproof, Bluetooth connectivity'
   },
   {
@@ -18,7 +18,7 @@ const ProductsData = [
     price: 49.99,
     discountedPrice: 39.99,
     discount: 20,
-    imgUrl: 'https://example.com/mens_denim_jeans.jpg',
+    imgUrl: 'https://ke.jumia.is/unsafe/fit-in/500x500/filters:fill(white)/product/58/0496471/1.jpg?8445',
     additionalParams: 'Slim fit, Available in various sizes and colors'
   },
   {
@@ -28,7 +28,7 @@ const ProductsData = [
     price: 79.99,
     discountedPrice: 59.99,
     discount: 25,
-    imgUrl: 'https://example.com/luxury_facial_cream.jpg',
+    imgUrl: 'https://ke.jumia.is/unsafe/fit-in/500x500/filters:fill(white)/product/73/8623941/1.jpg?9077',
     additionalParams: 'Anti-aging, Hydrating formula'
   },
   {
@@ -38,7 +38,7 @@ const ProductsData = [
     price: 149.99,
     discountedPrice: 119.99,
     discount: 20,
-    imgUrl: 'https://example.com/cookware_set.jpg',
+    imgUrl: 'https://images-cdn.ubuy.com.sa/64cd3d00a2d3c545434110b9-gotham-steel-12-piece-non-stick-cookware.jpg',
     additionalParams: 'Non-stick coating, Dishwasher safe'
   },
   {
@@ -48,7 +48,7 @@ const ProductsData = [
     price: 129.99,
     discountedPrice: 99.99,
     discount: 23,
-    imgUrl: 'https://example.com/camping_tent.jpg',
+    imgUrl: 'https://ke.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/03/6638661/1.jpg?3445',
     additionalParams: 'Waterproof, Lightweight design'
   },
   {
@@ -58,7 +58,7 @@ const ProductsData = [
     price: 39.99,
     discountedPrice: 29.99,
     discount: 25,
-    imgUrl: 'https://example.com/remote_control_car.jpg',
+    imgUrl: 'https://www.jiomart.com/images/product/original/493831479/frendo-red-abs-plastic-remote-control-car-toy-verc3458-product-images-o493831479-p604233475-0-202308301403.jpg?im=Resize=(420,420)',
     additionalParams: 'Rechargeable battery, High-speed capability'
   },
   {
@@ -68,7 +68,7 @@ const ProductsData = [
     price: 12.99,
     discountedPrice: 9.99,
     discount: 23,
-    imgUrl: 'https://example.com/the_last_adventure_novel.jpg',
+    imgUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROfKLoMTENf4Gi-9C41Xhu0LEPX5NtdRLD1QdafYeto4-P-vNPYdrf1YtD9wkwTUwu2jg&usqp=CAU',
     additionalParams: 'Author: John Doe, Hardcover edition'
   },
   {
@@ -78,7 +78,7 @@ const ProductsData = [
     price: 29.99,
     discountedPrice: 19.99,
     discount: 33,
-    imgUrl: 'https://example.com/electric_toothbrush.jpg',
+    imgUrl: 'https://skywave.co.ke/wp-content/uploads/2023/04/WhatsApp-Image-2023-04-19-at-13.35.50-1.jpeg',
     additionalParams: 'Includes replacement brush heads, Timer feature'
   },
   {
@@ -88,7 +88,7 @@ const ProductsData = [
     price: 149.99,
     discountedPrice: 129.99,
     discount: 13,
-    imgUrl: 'https://example.com/noise_cancelling_headphones.jpg',
+    imgUrl: 'https://www.masoko.com/_next/image?url=https%3A%2F%2Fassets.shop.masoko.com%2Fcatalog%2Fproduct%2Fh%2F6%2Fh630gt_pro_1_2_.png&w=640&q=75',
     additionalParams: 'Bluetooth 5.0, Foldable design'
   },
   {
@@ -98,7 +98,7 @@ const ProductsData = [
     price: 59.99,
     discountedPrice: 49.99,
     discount: 17,
-    imgUrl: 'https://example.com/womens_athletic_shoes.jpg',
+    imgUrl: 'https://image.kilimall.com/kenya/shop/store/goods/5991/2020/06/5991_06461767765843208.jpg.webp',
     additionalParams: 'Breathable mesh material, Available in various sizes'
   },
   {
@@ -108,7 +108,7 @@ const ProductsData = [
     price: 39.99,
     discountedPrice: 29.99,
     discount: 25,
-    imgUrl: 'https://example.com/vitamin_c_serum.jpg',
+    imgUrl: 'https://i0.wp.com/sterlingcosmetics.co.ke/wp-content/uploads/2022/09/BFF5F107-44F1-4B19-BFC5-786EB1BA1635.jpeg?resize=460%2C460&ssl=1',
     additionalParams: 'Brightening formula, Suitable for all skin types'
   },
   {
@@ -118,89 +118,91 @@ const ProductsData = [
     price: 199.99,
     discountedPrice: 159.99,
     discount: 20,
-    imgUrl: 'https://example.com/robot_vacuum_cleaner.jpg',
+    imgUrl: 'https://ke.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/45/4983451/1.jpg?4342',
     additionalParams: 'Wi-Fi connectivity, Voice control compatible'
-  },
-  {
-    id: 13,
-    category: 'Sports & Outdoors',
-    name: 'Yoga Mat with Carrying Strap',
-    price: 24.99,
-    discountedPrice: 19.99,
-    discount: 20,
-    imgUrl: 'https://example.com/yoga_mat.jpg',
-    additionalParams: 'Eco-friendly material, Non-slip surface'
-  },
-  {
-    id: 14,
-    category: 'Toys & Games',
-    name: 'LEGO Classic Creative Bricks Set (500 Pieces)',
-    price: 29.99,
-    discountedPrice: 24.99,
-    discount: 17,
-    imgUrl: 'https://example.com/lego_bricks_set.jpg',
-    additionalParams: 'Includes a variety of colorful bricks, Compatible with other LEGO sets'
-  },
-  {
-    id: 15,
-    category: 'Books',
-    name: 'Cookbook: "Healthy Eating Made Easy"',
-    price: 19.99,
-    discountedPrice: 14.99,
-    discount: 25,
-    imgUrl: 'https://example.com/healthy_eating_cookbook.jpg',
-    additionalParams: 'Author: Jane Smith, Paperback edition'
-  },
-  {
-    id: 16,
-    category: 'Health & Personal Care',
-    name: 'Aromatherapy Essential Oil Diffuser',
-    price: 34.99,
-    discountedPrice: 24.99,
-    discount: 29,
-    imgUrl: 'https://example.com/essential_oil_diffuser.jpg',
-    additionalParams: 'LED light changing, Timer settings'
-  },
-  {
-    id: 17,
-    category: 'Electronics',
-    name: '4K Ultra HD Smart TV (55-inch)',
-    price: 699.99,
-    discountedPrice: 599.99,
-    discount: 14,
-    imgUrl: 'https://example.com/smart_tv_4k_ultra_hd.jpg',
-    additionalParams: 'Built-in streaming apps, Voice remote included'
-  },
-  {
-    id: 18,
-    category: 'Clothing',
-    name: 'Unisex Beanie Hat',
-    price: 19.99,
-    discountedPrice: 14.99,
-    discount: 25,
-    imgUrl: 'https://example.com/beanie_hat.jpg',
-    additionalParams: 'One size fits all, Available in various colors'
-  },
-  {
-    id: 19,
-    category: 'Beauty',
-    name: 'Anti-Acne Facial Cleanser',
-    price: 24.99,
-    discountedPrice: 19.99,
-    discount: 20,
-    imgUrl: 'https://example.com/anti_acne_cleanser.jpg',
-    additionalParams: 'Salicylic acid, Oil-free formula'
-  },
-  {
-    id: 20,
-    category: 'Home & Kitchen',
-    name: 'Electric Kettle (1.7 Liters)',
-    price: 39.99,
-    discountedPrice: 29.99,
-    discount: 25,
-    imgUrl: 'https://example.com/electric_kettle.jpg',
-    additionalParams: 'Fast boil, Auto shut-off feature'
   },
 ];
 
 export default ProductsData;
+
+
+// {
+//   id: 13,
+//   category: 'Sports & Outdoors',
+//   name: 'Yoga Mat with Carrying Strap',
+//   price: 24.99,
+//   discountedPrice: 19.99,
+//   discount: 20,
+//   imgUrl: 'https://example.com/yoga_mat.jpg',
+//   additionalParams: 'Eco-friendly material, Non-slip surface'
+// },
+// {
+//   id: 14,
+//   category: 'Toys & Games',
+//   name: 'LEGO Classic Creative Bricks Set (500 Pieces)',
+//   price: 29.99,
+//   discountedPrice: 24.99,
+//   discount: 17,
+//   imgUrl: 'https://example.com/lego_bricks_set.jpg',
+//   additionalParams: 'Includes a variety of colorful bricks, Compatible with other LEGO sets'
+// },
+// {
+//   id: 15,
+//   category: 'Books',
+//   name: 'Cookbook: "Healthy Eating Made Easy"',
+//   price: 19.99,
+//   discountedPrice: 14.99,
+//   discount: 25,
+//   imgUrl: 'https://example.com/healthy_eating_cookbook.jpg',
+//   additionalParams: 'Author: Jane Smith, Paperback edition'
+// },
+// {
+//   id: 16,
+//   category: 'Health & Personal Care',
+//   name: 'Aromatherapy Essential Oil Diffuser',
+//   price: 34.99,
+//   discountedPrice: 24.99,
+//   discount: 29,
+//   imgUrl: 'https://example.com/essential_oil_diffuser.jpg',
+//   additionalParams: 'LED light changing, Timer settings'
+// },
+// {
+//   id: 17,
+//   category: 'Electronics',
+//   name: '4K Ultra HD Smart TV (55-inch)',
+//   price: 699.99,
+//   discountedPrice: 599.99,
+//   discount: 14,
+//   imgUrl: 'https://example.com/smart_tv_4k_ultra_hd.jpg',
+//   additionalParams: 'Built-in streaming apps, Voice remote included'
+// },
+// {
+//   id: 18,
+//   category: 'Clothing',
+//   name: 'Unisex Beanie Hat',
+//   price: 19.99,
+//   discountedPrice: 14.99,
+//   discount: 25,
+//   imgUrl: 'https://example.com/beanie_hat.jpg',
+//   additionalParams: 'One size fits all, Available in various colors'
+// },
+// {
+//   id: 19,
+//   category: 'Beauty',
+//   name: 'Anti-Acne Facial Cleanser',
+//   price: 24.99,
+//   discountedPrice: 19.99,
+//   discount: 20,
+//   imgUrl: 'https://example.com/anti_acne_cleanser.jpg',
+//   additionalParams: 'Salicylic acid, Oil-free formula'
+// },
+// {
+//   id: 20,
+//   category: 'Home & Kitchen',
+//   name: 'Electric Kettle (1.7 Liters)',
+//   price: 39.99,
+//   discountedPrice: 29.99,
+//   discount: 25,
+//   imgUrl: 'https://example.com/electric_kettle.jpg',
+//   additionalParams: 'Fast boil, Auto shut-off feature'
+// },
