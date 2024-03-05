ddf6d0a86870988533722692c391ad41a7e70cca
philipNgungi037 <philipngungi037@gmail.com>
2024-03-05 20:17:50 +0300

    :safety_vest::lipstick: Add width and height props. adjust padding.

--- src/components/landing-page/products/ProductCard.jsx
+++ src/components/landing-page/products/ProductCard.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 
 // A re-usable card with product prop, to be used to display single-item all over the app
-const ProductCard = ({ product }) => {
+const ProductCard = ({ product, width, height }) => {
   return (
     <div className="product-card bg-white shadow-md relative text-xs rounded-md ">
-      <img src={product.imgUrl} alt={product.name} className='relative bg-cover rounded-md  ' />
-      <p className='discount-tag  absolute top-0 rounded-md p-1 px-2    '><strong>Off -</strong> {product.discount}%</p>
-      <div className="details p-4 text-center ">
+      <img src={product.imgUrl}
+        alt={product.name}
+        // className='relative w-[300px] h-[200px]  bg-cover rounded-md  '
+        className={`relative w-[${width}] h-[${height}] bg-cover rounded-md m-0 w-full`}
+      />
+      <p className='discount-tag absolute top-0 rounded-md p-1 px-2'><strong>Off -</strong> {product.discount}%</p>
+      <div className="details p-4 text-center">
         <h2 className='font-extrabold  '>{product.name}</h2>
         <p className='underline '><strong></strong> {product.category}</p>
         <p className='text-sm'><strong> </strong> ${product.discountedPrice}</p>
