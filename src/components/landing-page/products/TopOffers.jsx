ec30d6cc5b9a2048658973998c6b690f9499d7c3
philipNgungi037 <philipngungi037@gmail.com>
2024-03-05 20:20:39 +0300

    :sparkles::art::lipstick: Create a top-offer components to dispay most discounted products.

---
+++ src/components/landing-page/products/TopOffers.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { useProductContext } from '../../contexts/product-context/ProductContext'
+import ProductCard from './ProductCard';
+
+const TopOffers = () => {
+
+    // This component displays top-offers products variable
+    const { TopOffers } = useProductContext();
+
+    return (
+        <div>
+            <div className='top-offers grid grid-cols-1 xs:grid-cols-2 lg:grid-cols-6  gap-4 px-16'>
+                <h1 className='top-offer-title bg-white p-2 w-full text-center uppercase  col-span-1 lg:col-span-6 rounded-md  '>
+                    top offers ({TopOffers.length})
+                </h1>
+                {
+                    TopOffers.map(
+                        (product) =>
+                            <ProductCard key={product.id} product={product} width="600px" height="200px" />
+                    )
+                }
+            </div>
+        </div>
+    )
+}
+
+export default TopOffers
