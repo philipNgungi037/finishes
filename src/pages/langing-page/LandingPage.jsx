e2e10e05959c75b89efa7c5d389de719516a6e1a
philipNgungi037 <philipngungi037@gmail.com>
2024-03-06 14:46:32 +0300

    :art: Render All-categories component into landing-page.

--- src/pages/langing-page/LandingPage.jsx
+++ src/pages/langing-page/LandingPage.jsx
@@ -3,11 +3,13 @@ import HeroSection from '../../components/landing-page/hero-section/HeroSection'
 import AllProducts from '../../components/landing-page/products/AllProducts'
 import '../../components/landing-page/products/products.css'
 import TopOffers from '../../components/landing-page/products/TopOffers'
+import AllCategories from '../../components/landing-page/products/AllCategories'
 
 const LandingPage = () => {
   return (
     <div className='landing-page bg-gray-200 h-full '>
       <HeroSection />
+      <AllCategories/>
       <TopOffers/>
       {/* <AllProducts/> */}
     </div>
