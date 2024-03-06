e93bb9609a06fd83ce22c198b9095f8f7f250751
philipNgungi037 <philipngungi037@gmail.com>
2024-03-06 11:35:55 +0300

    :art::fire: Make category have title and image key/value pairs. and remove unnecessary code.

--- src/components/landing-page/products/ProductsData.jsx
+++ src/components/landing-page/products/ProductsData.jsx
@@ -3,7 +3,10 @@
 const ProductsData = [
   {
     id: 1,
-    category: 'Electronics',
+    category: {
+      title: 'Electronics',
+      img: 'https://www.thenoiseuk.com/wp-content/uploads/2021/10/Consumer-Electronics-Appliance_blog-1024x701.jpeg'
+    },
     name: 'Smart Watch Series X',
     price: 99.99,
     discountedPrice: 79.99,
@@ -13,7 +16,10 @@ const ProductsData = [
   },
   {
     id: 2,
-    category: 'Clothing',
+    category: {
+      title: 'Clothing',
+      img: 'https://www.cato.org/sites/cato.org/files/styles/hero_impact_desktop/public/2023-11/fast-fashion2.jpeg?itok=9XNZrGt7'
+    },
     name: "Men's Classic Denim Jeans",
     price: 49.99,
     discountedPrice: 39.99,
@@ -23,7 +29,10 @@ const ProductsData = [
   },
   {
     id: 3,
-    category: 'Beauty',
+    category: {
+      title: 'Beauty',
+      img: 'https://static.thcdn.com/images/medium/original/widgets/290-en/34/HP_HERO_MOBILE_600x600_LIPS_MODEL-094534.jpg'
+    },
     name: 'Luxury Facial Cream',
     price: 79.99,
     discountedPrice: 59.99,
@@ -33,7 +42,10 @@ const ProductsData = [
   },
   {
     id: 4,
-    category: 'Home & Kitchen',
+    category: {
+      title: 'Home & Kitchen',
+      img: 'https://static.thcdn.com/images/medium/original/widgets/290-en/34/HP_HERO_MOBILE_600x600_LIPS_MODEL-094534.jpg'
+    },
     name: 'Stainless Steel Cookware Set (12-piece)',
     price: 149.99,
     discountedPrice: 119.99,
@@ -43,7 +55,10 @@ const ProductsData = [
   },
   {
     id: 5,
-    category: 'Sports & Outdoors',
+    category: {
+      title: 'Sports & Outdoors',
+      img: 'https://img.freepik.com/free-photo/sports-tools_53876-138077.jpg'
+    },
     name: 'Camping Tent for 4 Persons',
     price: 129.99,
     discountedPrice: 99.99,
@@ -53,7 +68,10 @@ const ProductsData = [
   },
   {
     id: 6,
-    category: 'Toys & Games',
+    category: {
+      title: 'Toys & Games',
+      img: 'https://previews.123rf.com/images/shangarey/shangarey1903/shangarey190300059/119054819-children-s-playroom-with-plastic-colorful-educational-blocks-toys-games-floor-for-preschoolers.jpg'
+    },
     name: 'Remote Control Car',
     price: 39.99,
     discountedPrice: 29.99,
@@ -63,7 +81,10 @@ const ProductsData = [
   },
   {
     id: 7,
-    category: 'Books',
+    category: {
+      title: 'Books',
+      img: 'https://discoveririshkidsbooks.ie/wp-content/uploads/2023/08/Irish-childrens-books-e1693410021475.jpeg'
+    },
     name: 'Bestselling Fiction Novel: "The Last Adventure"',
     price: 12.99,
     discountedPrice: 9.99,
@@ -73,7 +94,10 @@ const ProductsData = [
   },
   {
     id: 8,
-    category: 'Health & Personal Care',
+    category: {
+      title: 'Health & Personal Care',
+      img: 'https://www.careinsurance.com/upload_master/media/posts/July2022/personal-hygiene.jpg'
+    },
     name: 'Electric Toothbrush',
     price: 29.99,
     discountedPrice: 19.99,
@@ -83,7 +107,10 @@ const ProductsData = [
   },
   {
     id: 9,
-    category: 'Electronics',
+    category: {
+      title: 'Electronics',
+      img: 'https://www.thenoiseuk.com/wp-content/uploads/2021/10/Consumer-Electronics-Appliance_blog-1024x701.jpeg'
+    },
     name: 'Wireless Noise-Cancelling Headphones',
     price: 149.99,
     discountedPrice: 129.99,
@@ -93,7 +120,10 @@ const ProductsData = [
   },
   {
     id: 10,
-    category: 'Clothing',
+    category: {
+      title: 'Clothing',
+      img: 'https://www.cato.org/sites/cato.org/files/styles/hero_impact_desktop/public/2023-11/fast-fashion2.jpeg?itok=9XNZrGt7'
+    },
     name: "Women's Athletic Shoes",
     price: 59.99,
     discountedPrice: 49.99,
@@ -103,7 +133,10 @@ const ProductsData = [
   },
   {
     id: 11,
-    category: 'Beauty',
+    category: {
+      title: 'Beauty',
+      img: 'https://static.thcdn.com/images/medium/original/widgets/290-en/34/HP_HERO_MOBILE_600x600_LIPS_MODEL-094534.jpg'
+    },
     name: 'Organic Vitamin C Serum',
     price: 39.99,
     discountedPrice: 29.99,
@@ -113,7 +146,10 @@ const ProductsData = [
   },
   {
     id: 12,
-    category: 'Home & Kitchen',
+    category: {
+      title: 'Home & Kitchen',
+      img: 'https://images-na.ssl-images-amazon.com/images/G/01/img18/home/2021/Q4/Q4_Storefront_Flip/Kitchen/2021Q4_newnnow_tile_738x500.jpg'
+    },
     name: 'Robot Vacuum Cleaner',
     price: 199.99,
     discountedPrice: 159.99,
@@ -122,87 +158,4 @@ const ProductsData = [
     additionalParams: 'Wi-Fi connectivity, Voice control compatible'
   },
 ];
-
 export default ProductsData;
-
-
-// {
-//   id: 13,
-//   category: 'Sports & Outdoors',
-//   name: 'Yoga Mat with Carrying Strap',
-//   price: 24.99,
-//   discountedPrice: 19.99,
-//   discount: 20,
-//   imgUrl: 'https://example.com/yoga_mat.jpg',
-//   additionalParams: 'Eco-friendly material, Non-slip surface'
-// },
-// {
-//   id: 14,
-//   category: 'Toys & Games',
-//   name: 'LEGO Classic Creative Bricks Set (500 Pieces)',
-//   price: 29.99,
-//   discountedPrice: 24.99,
-//   discount: 17,
-//   imgUrl: 'https://example.com/lego_bricks_set.jpg',
-//   additionalParams: 'Includes a variety of colorful bricks, Compatible with other LEGO sets'
-// },
-// {
-//   id: 15,
-//   category: 'Books',
-//   name: 'Cookbook: "Healthy Eating Made Easy"',
-//   price: 19.99,
-//   discountedPrice: 14.99,
-//   discount: 25,
-//   imgUrl: 'https://example.com/healthy_eating_cookbook.jpg',
-//   additionalParams: 'Author: Jane Smith, Paperback edition'
-// },
-// {
-//   id: 16,
-//   category: 'Health & Personal Care',
-//   name: 'Aromatherapy Essential Oil Diffuser',
-//   price: 34.99,
-//   discountedPrice: 24.99,
-//   discount: 29,
-//   imgUrl: 'https://example.com/essential_oil_diffuser.jpg',
-//   additionalParams: 'LED light changing, Timer settings'
-// },
-// {
-//   id: 17,
-//   category: 'Electronics',
-//   name: '4K Ultra HD Smart TV (55-inch)',
-//   price: 699.99,
-//   discountedPrice: 599.99,
-//   discount: 14,
-//   imgUrl: 'https://example.com/smart_tv_4k_ultra_hd.jpg',
-//   additionalParams: 'Built-in streaming apps, Voice remote included'
-// },
-// {
-//   id: 18,
-//   category: 'Clothing',
-//   name: 'Unisex Beanie Hat',
-//   price: 19.99,
-//   discountedPrice: 14.99,
-//   discount: 25,
-//   imgUrl: 'https://example.com/beanie_hat.jpg',
-//   additionalParams: 'One size fits all, Available in various colors'
-// },
-// {
-//   id: 19,
-//   category: 'Beauty',
-//   name: 'Anti-Acne Facial Cleanser',
-//   price: 24.99,
-//   discountedPrice: 19.99,
-//   discount: 20,
-//   imgUrl: 'https://example.com/anti_acne_cleanser.jpg',
-//   additionalParams: 'Salicylic acid, Oil-free formula'
-// },
-// {
-//   id: 20,
-//   category: 'Home & Kitchen',
-//   name: 'Electric Kettle (1.7 Liters)',
-//   price: 39.99,
-//   discountedPrice: 29.99,
-//   discount: 25,
-//   imgUrl: 'https://example.com/electric_kettle.jpg',
-//   additionalParams: 'Fast boil, Auto shut-off feature'
-// },
