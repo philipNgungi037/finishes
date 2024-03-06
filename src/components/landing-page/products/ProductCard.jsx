f00b8a4694e491bc9619a8e8ed0000cbf778aeab
philipNgungi037 <philipngungi037@gmail.com>
2024-03-06 12:20:49 +0300

    :safety_vest: Add a function to round and convert prive.

--- src/components/landing-page/products/ProductCard.jsx
+++ src/components/landing-page/products/ProductCard.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 
 // A re-usable card with product prop, to be used to display single-item all over the app
 const ProductCard = ({ product, width, height }) => {
+
+    // Function to round off prices to the nearest whole number after multiplying by 100
+    const roundPrice = (price) => {
+      return Math.round(price * 100);
+    };
+  
   return (
     <div className="product-card bg-white shadow-md relative text-xs rounded-md ">
       <img src={product.imgUrl}
@@ -13,8 +19,8 @@ const ProductCard = ({ product, width, height }) => {
       <div className="details p-4 text-center">
         <h2 className='font-extrabold  '>{product.name}</h2>
         <p className='underline '><strong></strong> {product.category.title}</p>
-        <p className='text-sm'><strong> </strong> ${product.discountedPrice}</p>
-        <p className='line-through'><strong></strong> ${product.price}</p>
+        <p className='text-sm'><strong> </strong> Ksh{roundPrice(product.discountedPrice)}</p>
+        <p className='line-through'><strong></strong> Ksh{roundPrice(product.price)}</p>
         <p><strong></strong> {product.additionalParams}</p>
       </div>
     </div>
