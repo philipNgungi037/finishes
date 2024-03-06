4f996c953ef438d401351c8bb3d944dbaf5ab248
philipNgungi037 <philipngungi037@gmail.com>
2024-03-06 13:18:37 +0300

    :recycle::fire::safety_vest: Redefine AllCategories and i have well-comment to understand better.

--- src/components/contexts/product-context/ProductContext.jsx
+++ src/components/contexts/product-context/ProductContext.jsx
@@ -7,7 +7,17 @@ export const ProductProvider = ({ children }) => {
     // products categorized-varables that can be accessed anywhere in the app by use of `useContext`
     const products = ProductsData;
     const AllProducts = products;
-    const AllCategories = [...new Set(ProductsData.map(product => product.category.title))];
+
+    // use a Set named uniqueCategories to store unique category titles and iterate over each product in ProductsData, adding its category title to the uniqueCategories set.
+    // Then use Array.from() to convert the set to an array and then use map() to create an array of category objects with only the title and img properties.
+    const uniqueCategories = new Set();
+    ProductsData.forEach(product => uniqueCategories.add(product.category.title));
+    const AllCategories = Array.from(uniqueCategories).map(categoryTitle => {
+        const product = ProductsData.find(product => product.category.title === categoryTitle);
+        return product.category;
+    });
+
+
     const TopOffers = products.filter(product => product.discount > 20);
     const TopPrice = products.reduce((prev, current) => (prev.price > current.price ? prev : current));
     const ElectronicsProducts = products.filter(product => product.category.title === 'Electronics');
