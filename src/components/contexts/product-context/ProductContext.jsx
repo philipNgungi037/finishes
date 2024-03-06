c431e3d21b3cc8928ad776ea536593b647d42c7e
philipNgungi037 <philipngungi037@gmail.com>
2024-03-06 11:36:49 +0300

    :art: Factor in category pairs in the main product-context.

--- src/components/contexts/product-context/ProductContext.jsx
+++ src/components/contexts/product-context/ProductContext.jsx
@@ -7,11 +7,12 @@ export const ProductProvider = ({ children }) => {
     // products categorized-varables that can be accessed anywhere in the app by use of `useContext`
     const products = ProductsData;
     const AllProducts = products;
+    const AllCategories = [...new Set(ProductsData.map(product => product.category.title))];
     const TopOffers = products.filter(product => product.discount > 20);
     const TopPrice = products.reduce((prev, current) => (prev.price > current.price ? prev : current));
-    const ElectronicsProducts = products.filter(product => product.category === 'Electronics');
-    const ClothingProducts = products.filter(product => product.category === 'Clothing');
-    const BeautyProducts = products.filter(product => product.category === 'Beauty');
+    const ElectronicsProducts = products.filter(product => product.category.title === 'Electronics');
+    const ClothingProducts = products.filter(product => product.category.title === 'Clothing');
+    const BeautyProducts = products.filter(product => product.category.title === 'Beauty');
     const ExpensiveProducts = products.filter(product => product.price > 100);
     const AffordableProducts = products.filter(product => product.price < 50);
 
@@ -25,6 +26,7 @@ export const ProductProvider = ({ children }) => {
             BeautyProducts,
             ExpensiveProducts,
             AffordableProducts,
+            AllCategories
         }}>
             {children}
         </ProductContext.Provider>
