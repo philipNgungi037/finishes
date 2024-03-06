0abe5d86ec8d379f8f977b4934b7a78ce590c956
philipNgungi037 <philipngungi037@gmail.com>
2024-03-06 11:38:55 +0300

    :art:

--- src/components/landing-page/products/TopOffers.jsx
+++ src/components/landing-page/products/TopOffers.jsx
@@ -8,10 +8,10 @@ const TopOffers = () => {
     const { TopOffers } = useProductContext();
 
     return (
-        <div>
+        <div className='relative'>
             <div className='top-offers grid grid-cols-1 xs:grid-cols-2 lg:grid-cols-6  gap-4 px-16'>
-                <h1 className='top-offer-title bg-white p-2 w-full text-center uppercase  col-span-1 lg:col-span-6 rounded-md  '>
-                    top offers ({TopOffers.length})
+                <h1 className='top-offer-title bg-white p-2 w-full text-center uppercase  col-span-1 lg:col-span-6 rounded-md  ' >
+                    top offers
                 </h1>
                 {
                     TopOffers.map(
@@ -20,7 +20,7 @@ const TopOffers = () => {
                     )
                 }
             </div>
-        </div>
+        </div >
     )
 }
 
