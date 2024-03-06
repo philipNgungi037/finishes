a5ea9ac159f89cc8c96815ada2887427ae6494e1
philipNgungi037 <philipngungi037@gmail.com>
2024-03-06 19:21:29 +0300

    :art::lipstick: Style landing-page and render affordable and Clothes-Beauty component.

--- src/pages/langing-page/LandingPage.jsx
+++ src/pages/langing-page/LandingPage.jsx
@@ -3,13 +3,17 @@ import HeroSection from '../../components/landing-page/hero-section/HeroSection'
 import '../../components/landing-page/products/products.css'
 import TopOffers from '../../components/landing-page/products/TopOffers'
 import AllCategories from '../../components/landing-page/products/AllCategories'
+import Affordable from '../../components/landing-page/products/Affordable'
+import ClothesAndBeauty from '../../components/landing-page/products/ClothesAndBeauty'
 
 const LandingPage = () => {
   return (
-    <div className='landing-page bg-gray-200 h-full '>
+    <div className='landing-page bg-gray-200 h-full flex flex-col gap-4 '>
       <HeroSection />
       <AllCategories/>
       <TopOffers/>
+      <Affordable/>
+      <ClothesAndBeauty/>
       {/* <AllProducts/> */}
     </div>
   )
