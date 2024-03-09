e61928e6b98607bbf1b0f98cb3b19e62ad5593c0
philipNgungi037 <philipngungi037@gmail.com>
2024-03-10 00:59:00 +0300

    :sparkles: About us that renders description.

---
+++ src/components/landing-page/about-us/About-us.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Description from './Description'
+
+const AboutUs = () => {
+  return (
+    <div>
+        <Description/>
+    </div>
+  )
+}
+
+export default AboutUs
