9f61fa100b64fa9815b1cbb8c237fdfadcfdc48b
philipNgungi037 <philipngungi037@gmail.com>
2024-03-10 00:55:53 +0300

    :sparkles: Crate a data file for the about us.

---
+++ src/components/landing-page/about-us/about-us-data.jsx
@@ -0,0 +1,28 @@
+const description = [
+    {
+        paragraph1: "Kilimall, established in June 2014, stands as Kenya's premier online shopping destination. With a steadfast commitment to 'Enriching Lives for Africa,' it has become a cornerstone of e-commerce in the region."
+    },
+    {
+        paragraph2: "From its inception, Kilimall has strived to redefine the retail experience, offering a diverse range of products to cater to the varied needs of its customers. Whether it's electronics, fashion, home essentials, or beauty products, Kilimall provides a one-stop solution for consumers across Africa."
+    },
+    {
+        paragraph3: "Beyond just being an e-commerce platform, Kilimall actively contributes to the socio-economic development of the continent. Through job creation, supporting local suppliers, and fostering entrepreneurship, Kilimall is not just a business entity but a catalyst for positive change."
+    }
+];
+
+const features = [
+    {
+        icon: '<ion-icon name="cart-outline"></ion-icon>',
+        description: "Wide Range of Products"
+    },
+    {
+        icon: '<ion-icon name="cart-outline"></ion-icon>',
+        description: "Secure Payment Options"
+    },
+    {
+        icon: '<ion-icon name="cart-outline"></ion-icon>',
+        description: "Fast Delivery"
+    }
+];
+
+export { description, features };
