d1242f8eeecb61cd90da3ac9cb22c6059e47e7f1
philipNgungi037 <philipngungi037@gmail.com>
2024-03-10 00:57:50 +0300

    :sparkles: Create a description component to start with

---
+++ src/components/landing-page/about-us/Description.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { description } from './about-us-data'
+
+const Description = () => {
+  return (
+    <div>Description</div>
+  )
+}
+
+export default Description
