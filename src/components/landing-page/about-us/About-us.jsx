4d56e286f374d47e773a8c557faaf1eb905c24dc
philipNgungi037 <philipngungi037@gmail.com>
2024-03-10 12:27:48 +0300

    :fire: Remove features component from main about-us.

--- src/components/landing-page/about-us/About-us.jsx
+++ src/components/landing-page/about-us/About-us.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import Description from './Description'
-// import Features from './Features'
 
 const AboutUs = () => {
     return (
         <div className='about-us-main text-sm'>
-            {/* <Features /> */}
             <Description />
 
         </div>
