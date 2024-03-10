f7cbe8c1902a94911fe42afd0fe7b9aab8a6c314
philipNgungi037 <philipngungi037@gmail.com>
2024-03-10 12:02:07 +0300

    :pencil2: Edit icons and some description.

--- src/components/landing-page/about-us/about-us-data.jsx
+++ src/components/landing-page/about-us/about-us-data.jsx
@@ -1,26 +1,26 @@
 const description = [
     {
-        paragraph1: "Kilimall, established in June 2014, stands as Kenya's premier online shopping destination. With a steadfast commitment to 'Enriching Lives for Africa,' it has become a cornerstone of e-commerce in the region."
+        paragraph1: "No-logo, established in June 2014, stands as Kenya's premier online shopping destination. With a steadfast commitment to 'Enriching Lives for Africa,' it has become a cornerstone of e-commerce in the region."
     },
     {
-        paragraph2: "From its inception, Kilimall has strived to redefine the retail experience, offering a diverse range of products to cater to the varied needs of its customers. Whether it's electronics, fashion, home essentials, or beauty products, Kilimall provides a one-stop solution for consumers across Africa."
+        paragraph2: "From its inception, No-logo has strived to redefine the retail experience, offering a diverse range of products to cater to the varied needs of its customers. Whether it's electronics, fashion, home essentials, or beauty products, No-logo provides a one-stop solution for consumers across Africa."
     },
     {
-        paragraph3: "Beyond just being an e-commerce platform, Kilimall actively contributes to the socio-economic development of the continent. Through job creation, supporting local suppliers, and fostering entrepreneurship, Kilimall is not just a business entity but a catalyst for positive change."
+        paragraph3: "Beyond just being an e-commerce platform, No-logo actively contributes to the socio-economic development of the continent. Through job creation, supporting local suppliers, and fostering entrepreneurship, No-logo just a business entity but a catalyst for positive change."
     }
 ];
 
 const features = [
     {
-        icon: '<ion-icon name="cart-outline"></ion-icon>',
+        icon: '<ion-icon name="bag-handle-outline"></ion-icon>',
         description: "Wide Range of Products"
     },
     {
-        icon: '<ion-icon name="cart-outline"></ion-icon>',
+        icon: '<ion-icon name="lock-closed-outline"></ion-icon>',
         description: "Secure Payment Options"
     },
     {
-        icon: '<ion-icon name="cart-outline"></ion-icon>',
+        icon: '<ion-icon name="train-outline"></ion-icon>',
         description: "Fast Delivery"
     }
 ];
