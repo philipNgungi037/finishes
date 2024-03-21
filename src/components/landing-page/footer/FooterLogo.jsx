0f391e7a310f8e77e0d86692733cf81723d23d3b
philipNgungi037 <philipngungi037@gmail.com>
2024-03-21 10:52:26 +0300

    :sparkles::art::lipstick: Create a component for holding a footer logo.

---
+++ src/components/landing-page/footer/FooterLogo.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const FooterLogo = () => {
+  return (
+    <div className='footer-logo'>
+        <h1 className='logo-title line-through'>
+            No logo
+        </h1>
+
+    </div>
+  )
+}
+
+export default FooterLogo
