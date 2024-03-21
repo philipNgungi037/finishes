37c525a1fdc5bf10af4a0d9139285b7f50b109c8
philipNgungi037 <philipngungi037@gmail.com>
2024-03-21 10:46:50 +0300

    :sparkles: Create a footer data file and add payments icons

---
+++ src/components/landing-page/footer/footer-data.jsx
@@ -0,0 +1,16 @@
+const payments = [
+    {
+        name: 'm-pesa',
+        icon: ''
+    },
+    {
+        name: 'visa',
+        icon: ''
+    },
+    {
+        name: 'mastercard',
+        icon: ''
+    }
+
+]
+export default payments;
