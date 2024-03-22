fd33a5fe976f66d73263feb04aeaa7ef83618bd9
philipNgungi037 <philipngungi037@gmail.com>
2024-03-22 12:49:12 +0300

    :lipstick: Add padding to clothe and beaty component.

--- src/components/landing-page/products/ClothesAndBeauty.jsx
+++ src/components/landing-page/products/ClothesAndBeauty.jsx
@@ -5,14 +5,17 @@ import ProductCard from './ProductCard';
 const ClothesAndBeauty = () => {
     const { BeautyProducts, ClothingProducts } = useProductContext();
     return (
-        <div className='clothes-and-beauty  relative  bg-gray-400 mx-16 rounded-md'>
-            <div className='clothes-beauty-title '>
-                <h1 className='top-offer-title  p-2 w-full text-center uppercase  rounded-md  ' >
+        <div className='clothes-and-beauty  relative  bg-gray-400 mx-16 py-4 rounded-md'>
+            <div className='clothes-beauty-title p-2 '>
+                <h1 className='top-offer-title w-full text-center uppercase  rounded-md  ' >
                     beauty and clothing only
                 </h1>
                 {/* <h1 className='underline text-sm text-center p-2'>View All</h1>  */}
             </div>
-            <div className='flex justify-between  bg-gray-400 gap-4'>
+
+           
+            <div className='flex justify-between  bg-gray-400 gap-4 p-4'>
+                
                 <div className='beauty-grid grid grid-cols-2 gap-4 bg-gray-900 p-4 rounded-md'>
 
                     {
