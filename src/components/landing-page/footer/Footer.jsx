b6698da4421eb9bf3e1dfa8b564e6c16de620fae
philipNgungi037 <philipngungi037@gmail.com>
2024-03-22 11:24:23 +0300

    :art::sparkles::lipstick: Create a Footer that comprises all footer related component.

---
+++ src/components/landing-page/footer/Footer.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import FooterHeader from './FooterHeader'
+import ImportantLinks from './ImportantLinks'
+import FooterCustomer from './FooterCustomer'
+import SocialLinks from './SocialLinks'
+import Business from './Business'
+
+//  Footer is just an UI component that renders all footer related components.
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <FooterHeader />
+      <div className='flex justify-between mt-4 px-16' >
+        <ImportantLinks />
+        <FooterCustomer />
+        <Business />
+        <SocialLinks />
+      </div>
+      <h1 className='copy-right my-4 font-extralight text-center'>
+      Copyright 2024 <span className='line-through '>No logo</span>, All rights reserved.
+      </h1>
+    </div>
+  )
+}
+
+export default Footer
