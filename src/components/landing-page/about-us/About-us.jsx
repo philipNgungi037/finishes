571f8b404eb80ca57d3846057fdd933c35578951
philipNgungi037 <philipngungi037@gmail.com>
2024-03-22 12:06:49 +0300

    :lipstick: Add horizontal padding at about-us.

--- src/components/landing-page/about-us/About-us.jsx
+++ src/components/landing-page/about-us/About-us.jsx
@@ -3,7 +3,7 @@ import Description from './Description'
 
 const AboutUs = () => {
     return (
-        <div className='about-us-main text-sm'>
+        <div className='about-us-main text-sm px-16'>
             <Description />
 
         </div>
