8574e688370b0eefc117ae4f130f37eb65067cca
philipNgungi037 <philipngungi037@gmail.com>
2024-03-22 12:22:41 +0300

    :lipstick: Add horizontal padding at features.

--- src/components/landing-page/about-us/Features.jsx
+++ src/components/landing-page/about-us/Features.jsx
@@ -4,7 +4,7 @@ import { features } from './about-us-data';
 
 const Features = () => {
     return (
-        <div className='about-us-features flex justify-between bg-white'>
+        <div className='about-us-features flex justify-between bg-white px-16'>
             {features.map((feature, index) => (
                 <div className='about-us-inner text-s font-light p-4 bg-gray-100 rounded-md' key={index}>
                     <div className=' flex flex-col text-center gap-2 '>
