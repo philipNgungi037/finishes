e612af18aa38de94b2787d20c8992155c0bddbb1
philipNgungi037 <philipngungi037@gmail.com>
2024-03-27 20:21:24 +0300

    :iphone: Adjust rsponsiveness to affordable products.

--- src/components/landing-page/products/Affordable.jsx
+++ src/components/landing-page/products/Affordable.jsx
@@ -8,9 +8,9 @@ const Affordable = () => {
     const { AffordableProducts} = useProductContext();
 
     return (
-        <div className='relative bg-gray-300 mx-16 rounded-md'>
-            <div className='top-offers grid grid-cols-1 xs:grid-cols-2 lg:grid-cols-5  gap-4 px-16 py-4'>
-                <h1 className='top-offer-title  p-2 w-full text-center uppercase  col-span-1 lg:col-span-6 rounded-md  ' >
+        <div className='relative bg-gray-300 mx-2 lg:mx-16 rounded-md'>
+            <div className='top-offers grid grid-cols-2 xs:grid-cols-3 lg:grid-cols-6  gap-1 lg:gap-4 px-2 py-4'>
+                <h1 className='top-offer-title  p-2 w-full text-center uppercase  col-span-2 xs:col-span-3  lg:col-span-6 rounded-md  ' >
                     Most Affordable
                 </h1>
                 {
