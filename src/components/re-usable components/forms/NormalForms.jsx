96370c35342374697940bd2d0dd7a4e1f9d50627
philipNgungi037 <philipngungi037@gmail.com>
2024-03-27 23:37:16 +0300

    :art::safety_vest: Add props to our component.

--- src/components/re-usable components/forms/NormalForms.jsx
+++ src/components/re-usable components/forms/NormalForms.jsx
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from 'react'
 
-const NormalForms = () => {
+// component renders a form dynamically based on the provided fields. 
+// It accepts props; fields (an array of objects representing form fields), onSubmit (a function to handle form submission), 
+// buttonText (text to display on the submit button), 
+// className (additional CSS classes for styling), and error (an optional error message to display).
+const NormalForms = ({ fields, onSubmit, buttonText, className, error }) => {
   return (
     <div>NormalForms</div>
   )
