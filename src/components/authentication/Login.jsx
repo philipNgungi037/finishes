8327daf460eff75cd8648b0be086c21a509b9c3d
philipNgungi037 <philipngungi037@gmail.com>
2024-03-28 00:18:29 +0300

    :art::safety_vest::bulb: Import forms and fileds .

--- src/components/authentication/Login.jsx
+++ src/components/authentication/Login.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import NormalForms from './NormalForms'; // Importing the reusable form component
+import { loginFields } from './formFields'; // Importing the login form fields
 
 const Login = () => {
   return (
