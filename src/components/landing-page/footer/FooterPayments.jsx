2abf58a60e8d4f3726bcde88e2f7d857b5580e7a
philipNgungi037 <philipngungi037@gmail.com>
2024-03-27 21:17:27 +0300

    :iphone: Adjust rsponsiveness to footer-payments .

--- src/components/landing-page/footer/FooterPayments.jsx
+++ src/components/landing-page/footer/FooterPayments.jsx
@@ -4,15 +4,15 @@ import { payments } from './footer-data'
 //This component will be displaying payments related links. It will import the links from Footer-data File. then .map will be used to display each link.
 const FooterPayments = () => {
   return (
-    <div className='w-1/4 '>
-        <h1 className='payments-title text-center p-4'>
+    <div className='w-full xs:w-3/4 sm:w-1/2 lg:w-1/4 text-xs lg:text-sm '>
+        <h1 className='payments-title text-center p-1 lg:p-4'>
             Paymnets Methods
         </h1>
-        <div  className='footer-payments text-sm flex justify-between'>
+        <div  className='footer-payments flex justify-between'>
         {
             payments.map(
                 (payment, index) =>
-                <div key={index} className='border border-yellow-400 py-4 px-2 rounded-md'>
+                <div key={index} className='border border-yellow-400 py-1 lg:py-4 px-1 lg:px-2 rounded-md text-justify'>
                     {payment.name}
                 </div>
             )
