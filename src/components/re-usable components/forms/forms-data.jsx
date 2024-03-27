01a570fe55491f91ac1a9cb8ecfc6f673716b5dd
philipNgungi037 <philipngungi037@gmail.com>
2024-03-27 23:18:45 +0300

    :art: Create a data for forms fields.

---
+++ src/components/re-usable components/forms/forms-data.jsx
@@ -0,0 +1,13 @@
+// formFields
+
+export const loginFields = [
+    { label: 'Email', name: 'email' },
+    { label: 'Password', name: 'password', type: 'password' },
+];
+
+export const registerFields = [
+    { label: 'Username', name: 'username' },
+    { label: 'Email', name: 'email', type: 'email' },
+    { label: 'Password', name: 'password', type: 'password' },
+    { label: 'Confirm-Password', name: 'confirm-password', type: 'confirm-password' },
+];
