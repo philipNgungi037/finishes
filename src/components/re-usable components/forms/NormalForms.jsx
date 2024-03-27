fe4999151bd3f0575bd5fdd0579471fef9a3c894
philipNgungi037 <philipngungi037@gmail.com>
2024-03-27 23:40:30 +0300

    :art::safety_vest: DEfine states to our component.

--- src/components/re-usable components/forms/NormalForms.jsx
+++ src/components/re-usable components/forms/NormalForms.jsx
@@ -5,6 +5,12 @@ import React, { useState, useEffect } from 'react'
 // buttonText (text to display on the submit button), 
 // className (additional CSS classes for styling), and error (an optional error message to display).
 const NormalForms = ({ fields, onSubmit, buttonText, className, error }) => {
+
+  // Define initial form state based on the specific fields
+  const initialFormState = Object.fromEntries(fields.map((field) => [field.name, '']));
+  const [formData, setFormData] = useState(initialFormState);
+  const [formError, setFormError] = useState(error);
+  
   return (
     <div>NormalForms</div>
   )
