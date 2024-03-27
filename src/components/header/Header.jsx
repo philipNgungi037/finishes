e49a112699c91c86f06bce1365eace1a33c2ee62
philipNgungi037 <philipngungi037@gmail.com>
2024-03-27 18:36:49 +0300

    :iphone: Add responsiveness to logo text.

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,12 +6,22 @@ import Search from './Search'
 
 const Header = () => {
   return (
-    // All these below rendered scryRenderedComponentsWithType, make up the header.
-    <header className='header-main flex justify-between items-center mt-4 px-16 shadow-lg sticky top-0 z-50'>
-        <Logo/>
-        <Search/>
-        <NavList/> 
-    </header>
+    <>
+      {/* // All these below rendered scryRenderedComponentsWithType, make up the header. */}
+      <header className='header-main hidden bg-blue-400 lg:flex justify-between items-center mt-4 px-0 lg:px-16 shadow-lg sticky top-0 z-50'>
+        <Logo />
+        <Search />
+        <NavList />
+      </header>
+      <header className='header-main lg:hidden grid grid-cols-2 items-center bg-blue-400  '>
+        <Logo />
+
+        <NavList />
+        <div className='col-span-2 '>
+          <Search />
+        </div>
+      </header>
+    </>
   )
 }
 
--- src/components/header/Logo.jsx
+++ src/components/header/Logo.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Logo = () => {
     const logo = "No logo";
     return (
-        <div className='logo  text-4xl font-extrabold    rounded-md p-4'>
+        <div className='logo  text:sm lg:text-4xl font-extrabold     rounded-md p-4'>
             <h3 className='logo-text uppercase line-through '>
                 {logo}
             </h3>
