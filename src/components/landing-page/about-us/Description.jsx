6501c477b67758e9845806a2a471cae9ad001979
philipNgungi037 <philipngungi037@gmail.com>
2024-03-27 20:45:50 +0300

    :iphone: Adjust rsponsiveness to about-us description.

--- src/components/landing-page/about-us/Description.jsx
+++ src/components/landing-page/about-us/Description.jsx
@@ -6,13 +6,13 @@ import Features from './Features'
 const Description = () => {
     return (
         <div className='about-us-description bg-white rounded-md'>
-            <h1 className='description-title text-xl px-16 py-8 '>
+            <h1 className='description-title text-sm lg:text-xl px-2 lg:px-16 py-4 lg:py-8 '>
                About us
                 {/* <Logo /> */}
                
             </h1>
             <Features/>
-            <div className='description-inner px-16 py-8 text-justify   '>
+            <div className='description-inner px-2 lg:px-16 py-8 text-justify  text-xs lg:text-sm '>
                 {
                     description.map(
                         (paragraph, index) =>
