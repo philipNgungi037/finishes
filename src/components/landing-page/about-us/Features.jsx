15005c7a236481a9c92585eef0712bc3763f157d
philipNgungi037 <philipngungi037@gmail.com>
2024-03-27 20:45:29 +0300

    :iphone: Adjust rsponsiveness to about-us features.

--- src/components/landing-page/about-us/Features.jsx
+++ src/components/landing-page/about-us/Features.jsx
@@ -4,10 +4,10 @@ import { features } from './about-us-data';
 
 const Features = () => {
     return (
-        <div className='about-us-features flex justify-between bg-white px-16'>
+        <div className='about-us-features flex justify-between bg-white px-2 lg:px-16'>
             {features.map((feature, index) => (
-                <div className='about-us-inner text-s font-light p-4 bg-gray-100 rounded-md' key={index}>
-                    <div className=' flex flex-col text-center gap-2 '>
+                <div className='about-us-inner text-xs lg:text-sm font-light p-1 lg:p-4 bg-gray-100 rounded-md' key={index}>
+                    <div className=' flex flex-col text-center  gap-1 lg:gap-2 '>
                         <h1 dangerouslySetInnerHTML={{ __html: feature.icon }} className='' />
                         <p>{feature.description}</p>
                     </div>
