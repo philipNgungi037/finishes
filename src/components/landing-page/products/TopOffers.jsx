60cedda833278a983e65a25cd53a7ed94c428a67
philipNgungi037 <philipngungi037@gmail.com>
2024-03-27 18:49:41 +0300

    :iphone: Adjust rsponsiveness to top offers products.

--- src/components/landing-page/products/TopOffers.jsx
+++ src/components/landing-page/products/TopOffers.jsx
@@ -8,15 +8,17 @@ const TopOffers = () => {
     const { TopOffers } = useProductContext();
 
     return (
-        <div className='relative'>
-            <div className='top-offers grid grid-cols-1 xs:grid-cols-2 lg:grid-cols-6  gap-4 px-16'>
-                <h1 className='top-offer-title bg-white p-2 w-full text-center uppercase  col-span-1 lg:col-span-6 rounded-md  ' >
+        <div className=''>
+            <div className='top-offers grid grid-cols-2 xs:grid-cols-3 lg:grid-cols-6  gap-1 lg:gap-4 px-1 lg:px-16'>
+                <h1 className='top-offer-title bg-white p-2 w-full text-center uppercase  col-span-2 xs:col-span-3 lg:col-span-6 rounded-md  ' >
                     top offers
                 </h1>
                 {
                     TopOffers.map(
                         (product) =>
-                            <ProductCard key={product.id} product={product} width="600px" height="200px" />
+                            // <div className='flex justify-center items-center'>
+                                <ProductCard key={product.id} product={product} className />
+                            
                     )
                 }
             </div>
