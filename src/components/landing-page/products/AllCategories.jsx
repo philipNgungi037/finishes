c8d61cf4cc0b0ea5d11820f899ccff80691aed71
philipNgungi037 <philipngungi037@gmail.com>
2024-03-27 18:47:54 +0300

    :iphone: Adjust rsponsiveness to hero -section category section.

--- src/components/landing-page/products/AllCategories.jsx
+++ src/components/landing-page/products/AllCategories.jsx
@@ -4,13 +4,13 @@ import { useProductContext } from '../../contexts/product-context/ProductContext
 const AllCategories = () => {
     const { AllCategories } = useProductContext();
 
-    console.log(AllCategories);
+    // console.log(AllCategories);
 
     return (
-        <div className='all-categories-main px-16 mb-4 bg-white flex gap-4 mx-16 rounded-md  overflow-x-scroll'>
+        <div className='all-categories-main px-0 lg:px-16 mb-4 bg-white flex justify-between gap-2 lg:gap-4 mx-0 lg:mx-16 rounded-md  overflow-x-scroll'>
             {AllCategories.map((category, index) => (
-                <div key={index} className="category-item mt-4 font-light  text-sm text-center">
-                    <img src={category.img} alt={category.title} className='w-[200px] h-[200px] rounded-md ' />
+                <div key={index} className="category-item mt-4 font-light text-xs lg:text-sm text-center">
+                    <img src={category.img} alt={category.title} className=' xs:h-[100px] sm:h-[150px]  rounded-md bg-cover' />
                     <h3>{category.title}</h3>
                 </div>
             ))}
