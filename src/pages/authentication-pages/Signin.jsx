c2a06aed50bd8a4b3e0e995218d85b2007a625c1
philipNgungi037 <philipngungi037@gmail.com>
2024-03-28 18:54:53 +0300

    :art::lipstick: Import logo and login + add title to to sigin page.

--- src/pages/authentication-pages/Signin.jsx
+++ src/pages/authentication-pages/Signin.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import Logo from '../../components/header/Logo'
+import Login from '../../components/authentication/Login'
 
 const Signin = () => {
   return (
-    <div className='signin-page w-full'>
-        
+    <div className='signin-page w-full h-full bg-gray-200'>
+      <Logo />
+      <h1 className='signin-title p-4 text-center'>
+        Access your account.
+      </h1>
+      <Login />
     </div>
   )
 }
