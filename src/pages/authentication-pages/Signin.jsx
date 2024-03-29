a72bdd6793aafde5cb2118f9d221afac40312cfb
philipNgungi037 <philipngungi037@gmail.com>
2024-03-29 12:34:18 +0300

    :safety_vest::recycle: Define on-submit func at parent signin-page.

--- src/pages/authentication-pages/Signin.jsx
+++ src/pages/authentication-pages/Signin.jsx
@@ -3,13 +3,19 @@ import Logo from '../../components/header/Logo'
 import Login from '../../components/authentication/Login'
 
 const Signin = () => {
+
+      // Define the onSubmit function to handle form submission
+      const handleSubmit = (formData) => {
+        // Logic to handle form submission
+        console.log(formData);
+    };
   return (
     <div className='signin-page w-full h-full bg-gray-200'>
       <Logo />
       <h1 className='signin-title p-4 text-center'>
         Access your account.
       </h1>
-      <Login />
+      <Login onSubmit={handleSubmit} />
     </div>
   )
 }
