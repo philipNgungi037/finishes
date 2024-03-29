6767bdd3ec4e3043748bec990d30230dec4c397c
philipNgungi037 <philipngungi037@gmail.com>
2024-03-29 12:20:21 +0300

    :art::lipstick: Structure and style the signup page.

--- src/pages/authentication-pages/Signup.jsx
+++ src/pages/authentication-pages/Signup.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 import Logo from '../../components/header/Logo'
 import Register from '../../components/authentication/Register'
 
+// This component will be rendering all signup/register UIs
 const Signup = () => {
   return (
-    <div>Signup</div>
+    <div className='signup-page  w-full h-full bg-gray-200'>
+        <Logo/>
+        <h1 className='signin-title p-4 text-center'>
+        Access your account.
+      </h1>
+      <Register/>
+
+    </div>
   )
 }
 
