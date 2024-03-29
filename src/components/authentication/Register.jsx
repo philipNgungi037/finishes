acb33d4a7e976dd50ff7b12b60c1b3f004330c68
philipNgungi037 <philipngungi037@gmail.com>
2024-03-29 12:12:45 +0300

    :art::lipstick::bulb: Structure, style and explain the register component.

--- src/components/authentication/Register.jsx
+++ src/components/authentication/Register.jsx
@@ -4,14 +4,23 @@ import { registerFields } from '../re-usable components/forms/forms-data'; // Im
 
 const Register = () => {
 
-      // Define the onSubmit function to handle form submission
+    // Define the onSubmit function to handle form submission
     const handleSubmit = (formData) => {
         // Handle form submission logic here
         console.log("Form submitted with data:", formData);
-      };
-  return (
-    <div>Register</div>
-  )
+    };
+    return (
+        <div className='register mx-auto'>
+       
+        {/* Reusing the NormalForms component for rendering the login form */}
+            <NormalForms
+                fields={registerFields} // Passes register form fields imported from formFields.js
+                onSubmit={handleSubmit} // Passes handleSubmit function to handle form submission
+                buttonText="Register" // Text to display on the submit button
+                className="bg-gray-100" // Additional CSS class for styling the form
+            />
+        </div>
+    )
 }
 
 export default Register
