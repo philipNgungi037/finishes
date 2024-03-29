526dbaf57c11d9f2f172089ef44b7cde32b5d33a
philipNgungi037 <philipngungi037@gmail.com>
2024-03-29 12:03:22 +0300

    :safety_vest: Import reusable forms and register fields.

--- src/components/authentication/Register.jsx
+++ src/components/authentication/Register.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import NormalForms from '../re-usable components/forms/NormalForms'; // Importing the reusable form component
+import { registerFields } from '../re-usable components/forms/forms-data'; // Importing the login form fields
 
 const Register = () => {
   return (
