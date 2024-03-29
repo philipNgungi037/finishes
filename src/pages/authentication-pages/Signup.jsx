f35a5c0329401b53681431246d782b10592e3039
philipNgungi037 <philipngungi037@gmail.com>
2024-03-29 12:29:48 +0300

    :safety_vest::recycle: Define on-submit func at parent signup-page.

--- src/pages/authentication-pages/Signup.jsx
+++ src/pages/authentication-pages/Signup.jsx
@@ -4,13 +4,18 @@ import Register from '../../components/authentication/Register'
 
 // This component will be rendering all signup/register UIs
 const Signup = () => {
+        // Define the onSubmit function to handle form submission
+        const handleSubmit = (formData) => {
+            // Handle form submission logic here
+            console.log("Form submitted with data:", formData);
+        };
   return (
     <div className='signup-page  w-full h-full bg-gray-200'>
         <Logo/>
         <h1 className='signin-title p-4 text-center'>
         Access your account.
       </h1>
-      <Register/>
+      <Register onSubmit={handleSubmit}/>
 
     </div>
   )
