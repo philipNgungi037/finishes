30ee7170fe76bfed0a57b3b0d07b5f156fdcb642
philipNgungi037 <philipngungi037@gmail.com>
2024-03-29 12:07:27 +0300

    :safety_vest::bulb: Submission function.

--- src/components/authentication/Register.jsx
+++ src/components/authentication/Register.jsx
@@ -3,6 +3,12 @@ import NormalForms from '../re-usable components/forms/NormalForms'; // Importin
 import { registerFields } from '../re-usable components/forms/forms-data'; // Importing the login form fields
 
 const Register = () => {
+
+      // Define the onSubmit function to handle form submission
+    const handleSubmit = (formData) => {
+        // Handle form submission logic here
+        console.log("Form submitted with data:", formData);
+      };
   return (
     <div>Register</div>
   )
