12285b33f94bb3956780a7d899a93ec5bc631a26
philipNgungi037 <philipngungi037@gmail.com>
2024-03-29 12:28:45 +0300

    :fire: Remove handleSubmit func and use it as prop

--- src/components/authentication/Register.jsx
+++ src/components/authentication/Register.jsx
@@ -2,20 +2,15 @@ import React from 'react'
 import NormalForms from '../re-usable components/forms/NormalForms'; // Importing the reusable form component
 import { registerFields } from '../re-usable components/forms/forms-data'; // Importing the login form fields
 
-const Register = () => {
+// use onSubmit as prop from signup-page
+const Register = ({ onSubmit }) => {
 
-    // Define the onSubmit function to handle form submission
-    const handleSubmit = (formData) => {
-        // Handle form submission logic here
-        console.log("Form submitted with data:", formData);
-    };
     return (
         <div className='register mx-auto'>
-       
-        {/* Reusing the NormalForms component for rendering the login form */}
+            {/* Reusing the NormalForms component for rendering the login form */}
             <NormalForms
                 fields={registerFields} // Passes register form fields imported from formFields.js
-                onSubmit={handleSubmit} // Passes handleSubmit function to handle form submission
+                onSubmit={onSubmit} // Passes handleSubmit function to handle form submission
                 buttonText="Register" // Text to display on the submit button
                 className="bg-gray-100" // Additional CSS class for styling the form
             />
