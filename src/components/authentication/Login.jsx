bf8b3541877a09d841bd745f26909be5d806ced4
philipNgungi037 <philipngungi037@gmail.com>
2024-03-29 12:32:48 +0300

    :fire::pencil2: Remove on-submit func from login

--- src/components/authentication/Login.jsx
+++ src/components/authentication/Login.jsx
@@ -2,21 +2,17 @@ import React from 'react'
 import NormalForms from '../re-usable components/forms/NormalForms'; // Importing the reusable form component
 import { loginFields } from '../re-usable components/forms/forms-data'; // Importing the login form fields
 
-// This component, accepts onSubmit as a prop
+// This component, accepts onSubmit as a prop from parent signin-page
 const Login = ({ onSubmit }) => {
 
-    // Define the onSubmit function to handle form submission
-    const handleSubmit = (formData) => {
-        // Logic to handle form submission
-        console.log(formData);
-    };
+
     return (
         <div className="container mx-auto"> 
    
             {/* Reusing the NormalForms component for rendering the login form */}
             <NormalForms
                 fields={loginFields} // Passes login form fields imported from formFields.js
-                onSubmit={handleSubmit} // Passes onSubmit function to handle form submission
+                onSubmit={onSubmit} // Passes onSubmit function to handle form submission
                 buttonText="Login" // Text to display on the submit button
                 className="login-form" // Additional CSS class for styling the form
             />
