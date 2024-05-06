653c52e26d0c8a949372b51a06abab43a62d64e3
philipNgungi037 <philipngungi037@gmail.com>
2024-05-07 02:48:58 +0300

    :pencil: Fix typo of login title.

--- src/pages/authentication-pages/Signin.jsx
+++ src/pages/authentication-pages/Signin.jsx
@@ -13,7 +13,7 @@ const Signin = () => {
     <div className='signin-page w-full h-full bg-gray-200'>
       <Logo />
       <h1 className='signin-title p-4 text-center'>
-        Access your account.
+        Access your account
       </h1>
       <Login onSubmit={handleSubmit} />
     </div>
