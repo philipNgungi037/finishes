3308ce4b5e84d3bece99b3a2c821e12454faf9a7
philipNgungi037 <philipngungi037@gmail.com>
2024-05-07 02:42:30 +0300

    :art: Render header inside labding-page.

--- src/pages/langing-page/LandingPage.jsx
+++ src/pages/langing-page/LandingPage.jsx
@@ -7,10 +7,12 @@ import Affordable from '../../components/landing-page/products/Affordable'
 import ClothesAndBeauty from '../../components/landing-page/products/ClothesAndBeauty'
 import AboutUs from '../../components/landing-page/about-us/About-us'
 import Footer from '../../components/landing-page/footer/Footer'
+import Header from '../../components/header/Header'
 
 const LandingPage = () => {
   return (
     <div className='landing-page bg-gray-200 h-full flex flex-col gap-4 '>
+      <Header/>
       <HeroSection />
       <AllCategories/>
       <TopOffers/>
