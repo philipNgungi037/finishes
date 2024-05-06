4a1fd9e46b3e450227b86c690f18ffdace628848
philipNgungi037 <philipngungi037@gmail.com>
2024-05-07 02:40:57 +0300

    :art::safety_vest: Introduce navLink from router-dom in logo to be used as home.

--- src/components/header/Logo.jsx
+++ src/components/header/Logo.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom';
 const Logo = () => {
     const logo = "No logo";
     return (
-        <div className='logo  text:sm lg:text-4xl font-extrabold     rounded-md p-4'>
-            <h3 className='logo-text uppercase line-through '>
-                {logo}
-            </h3>
-        </div>
+        <Link to='/'>
+            <div className='logo  text:sm lg:text-4xl font-extrabold     rounded-md p-4'>
+                <h3 className='logo-text uppercase line-through '>
+                    {logo}
+                </h3>
+            </div>
+        </Link>
+
     )
 }
 
