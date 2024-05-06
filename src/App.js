941bd10ed84a04157f519354e94a46f4317ef603
philipNgungi037 <philipngungi037@gmail.com>
2024-05-07 02:44:42 +0300

    :art::safety_vest: Route LandingPage and signin pages in main app.

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import { ProductProvider } from './components/contexts/product-context/ProductCo
 import Header from './components/header/Header';
 import LandingPage from './pages/langing-page/LandingPage';
 import { Route, Routes } from "react-router";
+import Signin from './pages/authentication-pages/Signin'
 
 function App() {
   return (
     <ProductProvider>
       <div className="app-main">
-        <Header />
-        <LandingPage />
+        {/* <Header /> */}
+        <Routes>
+          <Route path='/' element={<LandingPage/>} />
+          <Route path='signin' element={<Signin />} />
+          
+        </Routes>
+        {/* <LandingPage /> */}
       </div>
     </ProductProvider>
   );
