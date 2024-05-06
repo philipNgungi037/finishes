2e45bea9873d9c1a57126e3fff079761fe24e9b7
philipNgungi037 <philipngungi037@gmail.com>
2024-05-07 02:38:39 +0300

    :pencil: Adjust paths on navs.

--- src/components/header/NavData.jsx
+++ src/components/header/NavData.jsx
@@ -1,25 +1,27 @@
 const Navs = [
   
     {
+        id: 1,
         name:'Account',
-        link: 'account',
+        link: 'signin',
         icon: '<ion-icon name="person-outline"></ion-icon>',
-        subnavs: [
-            {
-            name: 'sign-up',
-            link: '',
-            icon: ''
-        },
-        {
-            name: 'sign-in',
-            link: '',
-            icon: ''
-        }
-    ]
+    //     subnavs: [
+    //         {
+    //         name: 'sign-up',
+    //         link: '',
+    //         icon: ''
+    //     },
+    //     {
+    //         name: 'sign-in',
+    //         link: '',
+    //         icon: ''
+    //     }
+    // ]
         
         
     },
     {
+        id: 2,
         name: 'Cart',
         link: 'cart',
         icon: '<ion-icon name="cart-outline"></ion-icon>'
