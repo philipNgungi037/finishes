6476364c1afeb9c7d0aa2c4cb9899d471e164581
philipNgungi037 <philipngungi037@gmail.com>
2024-06-17 12:37:33 +0300

    :lipstick: Adjust styles to signin page.

--- src/pages/authentication-pages/Signin.jsx
+++ src/pages/authentication-pages/Signin.jsx
@@ -4,18 +4,22 @@ import Login from '../../components/authentication/Login'
 
 const Signin = () => {
 
-      // Define the onSubmit function to handle form submission
-      const handleSubmit = (formData) => {
-        // Logic to handle form submission
-        console.log(formData);
-    };
+  // Define the onSubmit function to handle form submission
+  const handleSubmit = (formData) => {
+    // Logic to handle form submission
+    console.log(formData);
+  };
   return (
-    <div className='signin-page w-full h-full bg-gray-200'>
-      <Logo />
-      <h1 className='signin-title p-4 text-center'>
-        Access your account
-      </h1>
-      <Login onSubmit={handleSubmit} />
+    <div className='signin-page w-full h-auto bg-gray-200'>
+        <div className='signin-logo'>
+          <Logo />
+          <h1 className='signin-title p-4 text-center'>
+            Access your account
+          </h1>
+        </div>
+      <div className='signin-login-container flex justify-center items-center '>
+        <Login onSubmit={handleSubmit} />
+      </div>
     </div>
   )
 }
