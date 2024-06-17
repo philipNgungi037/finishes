184412330f89a5a6c636f79893075a19b7996913
philipNgungi037 <philipngungi037@gmail.com>
2024-06-17 12:36:06 +0300

    :lipstick: Adjust text styles to the logo.

--- src/components/header/Logo.jsx
+++ src/components/header/Logo.jsx
@@ -4,7 +4,7 @@ const Logo = () => {
     const logo = "No logo";
     return (
         <Link to='/'>
-            <div className='logo  text:sm lg:text-4xl font-extrabold     rounded-md p-4'>
+            <div className='logo text-center  text:sm lg:text-4xl font-extrabold     rounded-md p-4'>
                 <h3 className='logo-text uppercase line-through '>
                     {logo}
                 </h3>
