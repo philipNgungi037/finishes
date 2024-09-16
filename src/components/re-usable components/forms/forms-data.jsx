5a46e01ec875bc933c079b72cf60f534c422014d
philipNgungi037 <philipngungi037@gmail.com>
2024-09-16 16:55:52 +0300

    :lipstick: adjust signup form fields.

--- src/components/re-usable components/forms/forms-data.jsx
+++ src/components/re-usable components/forms/forms-data.jsx
@@ -9,5 +9,5 @@ export const registerFields = [
     { label: 'Username', name: 'username' },
     { label: 'Email', name: 'email', type: 'email' },
     { label: 'Password', name: 'password', type: 'password' },
-    { label: 'Confirm-Password', name: 'confirm-password', type: 'confirm-password' },
+    
 ];
