dd013f74085078ce738e9d1b230a61ebbbdcc6ad
philipNgungi037 <philipngungi037@gmail.com>
2024-09-18 13:32:45 +0300

    :zap: Introduce new doc, set doc to access firestore direct.

--- src/components/contexts/AuthContext.jsx
+++ src/components/contexts/AuthContext.jsx
@@ -1,6 +1,7 @@
 import React, { createContext, useContext, useState } from 'react';
 import { auth, fs } from '../../config/Firebase';
 import { createUserWithEmailAndPassword } from '@firebase/auth';
+import { collection, doc, setDoc } from 'firebase/firestore'; 
 
 // Create context
 const AuthContext = createContext();
@@ -13,18 +14,25 @@ export const AuthProvider = ({ children }) => {
   const [errorMsg, setErrorMsg] = useState('');
   const [successMsg, setSuccessMsg] = useState('');
 
+ 
   const signup = async ({ username, email, password }) => {
     try {
       const credentials = await createUserWithEmailAndPassword(auth, email, password);
-      await fs.collection('users').doc(credentials.user.uid).set({
+      
+      const usersCollection = collection(fs, 'users'); // Reference to 'users' collection
+      const userDoc = doc(usersCollection, credentials.user.uid); // Reference to the user's document
+      
+      await setDoc(userDoc, {
         username: username,
         email: email,
       });
+      
       setSuccessMsg('User created successfully');
     } catch (error) {
       setErrorMsg(error.message);
     }
   };
+  
 
   return (
     <AuthContext.Provider value={{ signup, successMsg, errorMsg }}>
