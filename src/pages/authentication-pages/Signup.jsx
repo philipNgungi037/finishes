ab557c118c466626b82d2ebbc7b38e7901fa1675
philipNgungi037 <philipngungi037@gmail.com>
2024-09-18 12:50:41 +0300

    :art: Define the signup and its error/succes message.

--- src/pages/authentication-pages/Signup.jsx
+++ src/pages/authentication-pages/Signup.jsx
@@ -1,25 +1,28 @@
 import React from 'react'
 import Logo from '../../components/header/Logo'
 import Register from '../../components/authentication/Register'
-import { useAuth } from '../../components/contexts/AuthContext'; 
+import { useAuth } from '../../components/contexts/AuthContext';
 
 // This component will be rendering all signup/register UIs
 const Signup = () => {
-        // Define the onSubmit function to handle form submission
-        const handleSubmit = (formData) => {
-            // Handle form submission logic here
-            console.log("Form submitted with data:", formData);
-        };
-  return (
-    <div className='signup-page  w-full h-full bg-gray-200 '>
-        <Logo/>
-        <h1 className='signin-title p-4 text-center'>
-        Create an Account
-      </h1>
-      <Register onSubmit={handleSubmit}  />
 
-    </div>
-  )
+    //signup variables
+    const { signup, successMsg, errorMsg } = useAuth();
+    // Define the onSubmit function to handle form submission
+    const handleSubmit = (formData) => {
+        // Handle form submission logic here
+        console.log("Form submitted with data:", formData);
+    };
+    return (
+        <div className='signup-page  w-full h-full bg-gray-200 '>
+            <Logo />
+            <h1 className='signin-title p-4 text-center'>
+                Create an Account
+            </h1>
+            <Register onSubmit={handleSubmit} />
+
+        </div>
+    )
 }
 
 export default Signup
