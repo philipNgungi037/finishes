8f1c7f1c835fec5b892ae3af2a0ddef5e285c1bc
philipNgungi037 <philipngungi037@gmail.com>
2024-09-19 15:43:23 +0300

    :sparkles::art: Apply login function in our signin page.

--- src/pages/authentication-pages/Signin.jsx
+++ src/pages/authentication-pages/Signin.jsx
@@ -1,25 +1,32 @@
 import React from 'react'
 import Logo from '../../components/header/Logo'
 import Login from '../../components/authentication/Login'
+import { useAuth } from '../../components/contexts/AuthContext'
 
 const Signin = () => {
+  // Use the login function from the AuthContext
+  const { login, errorMsg, successMsg } = useAuth();
 
   // Define the onSubmit function to handle form submission
   const handleSubmit = (formData) => {
-    // Logic to handle form submission
-    console.log(formData);
+
+    // Call login function from AuthContext
+    login(formData);
   };
   return (
     <div className='signin-page w-full h-auto bg-gray-200'>
-        <div className='signin-logo'>
-          <Logo />
-          <h1 className='signin-title p-4 text-center'>
-            Access your account
-          </h1>
-        </div>
+      <div className='signin-logo'>
+        <Logo />
+        <h1 className='signin-title p-4 text-center'>
+          Access your account
+        </h1>
+      </div>
       <div className='signin-login-container flex justify-center items-center '>
         <Login onSubmit={handleSubmit} />
       </div>
+      {/* Display success or error messages */}
+      {successMsg && <p className="text-green-500 text-center mt-4">{successMsg}</p>}
+      {errorMsg && <p className="text-red-500 text-center mt-4">{errorMsg}</p>}
     </div>
   )
 }
