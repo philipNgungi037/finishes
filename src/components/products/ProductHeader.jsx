657cabc8a0b3e3db0212096f55b5e8a4a2043e2a
philipNgungi037 <philipngungi037@gmail.com>
2024-09-27 17:36:39 +0300

    :art: create product-header component to have img ant it title.

--- src/components/products/ProductHeader.jsx
+++ src/components/products/ProductHeader.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
-
+//**this component will have the image slider and product titles**/
 const ProductHeader = () => {
   return (
-    <div>ProductHeader</div>
+    <div className='product-header w-full flex '>
+        <div className='product-header-img'>
+
+        </div>
+        <div className='product-header-title'>
+
+        </div>
+
+    </div>
   )
 }
 
