7ffdd72fdbd3773ef3a5502752090f737555e371
philipNgungi037 <philipngungi037@gmail.com>
2024-10-16 22:27:53 +0300

    :art: add a loading state inside our signin page.

--- src/pages/authentication-pages/Signin.jsx
+++ src/pages/authentication-pages/Signin.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import Logo from '../../components/header/Logo'
 import Login from '../../components/authentication/Login'
 import { useAuth } from '../../components/contexts/AuthContext'
+import { ClipLoader } from "react-spinners";
 
 const Signin = () => {
   // Use the login function from the AuthContext
-  const { login, errorMsg, successMsg } = useAuth();
+  const { login, errorMsg, LoginError, loading, successMsg } = useAuth();
 
   // Define the onSubmit function to handle form submission
   const handleSubmit = (formData) => {
@@ -22,11 +23,18 @@ const Signin = () => {
         </h1>
       </div>
       <div className='signin-login-container flex justify-center items-center '>
+        { loading ? (
+        <div className="flex justify-center items-center">
+          <ClipLoader color="#000" loading={true} size={35} /> {/* Render the ClipLoader component */}
+        </div>
+      ) : (
         <Login onSubmit={handleSubmit} />
+      )
+        }
       </div>
       {/* Display success or error messages */}
       {successMsg && <p className="text-green-500 text-center mt-4">{successMsg}</p>}
-      {errorMsg && <p className="text-red-500 text-center mt-4">{errorMsg}</p>}
+      {errorMsg && <p className="text-red-500 text-center mt-4">{LoginError}</p>}
     </div>
   )
 }
