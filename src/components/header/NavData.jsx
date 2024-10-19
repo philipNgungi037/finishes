4ebe9f733b25e63315cc792e8c7c0acad6411cfb
philipNgungi037 <philipngungi037@gmail.com>
2024-10-19 14:39:12 +0300

    :art: add sample user's subnavs.

--- src/components/header/NavData.jsx
+++ src/components/header/NavData.jsx
@@ -1,16 +1,21 @@
 const Navs = (username) => [
-  
     {
         id: 1,
-        name: username ? username : 'Account', // Conditionally show username or 'Account'
-        link: username ? 'profile' : 'signin', // Link to profile if logged in, otherwise signin
-        icon: '<ion-icon name="person-outline"></ion-icon>',  
+        name: username ? username : 'Account',
+        link: username ? 'profile' : 'signin',
+        icon: '<ion-icon name="person-outline"></ion-icon>',
+        //to be displayed by the account/user dropdown.
+        subnavs: username ? [
+            { name: 'Orders', link: '/orders' },
+            { name: 'Profile', link: '/profile' },
+            { name: 'Saved Items', link: '/saved-items' }
+        ] : null, 
     },
     {
         id: 2,
         name: 'Cart',
         link: 'cart',
-        icon: '<ion-icon name="cart-outline"></ion-icon>'
-    }
+        icon: '<ion-icon name="cart-outline"></ion-icon>',
+    },
 ];
 export default Navs;
