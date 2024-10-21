4999e85c0827f7261284fc88ef903e8aa60119ee
philipNgungi037 <philipngungi037@gmail.com>
2024-10-21 17:24:05 +0300

    :art: add background color variant.

--- tailwind.config.js
+++ tailwind.config.js
@@ -39,7 +39,7 @@ module.exports = {
       //this will be our global colors
       colors: {
         appColor: "yellow", 
-        background: "", 
+        bgColor: "#e5e7eb", 
       },
       /**Fonts  */
       fontFace: {
